480d82c6f8ed1e1fae854eaf025696608d5753c1
alabak219 <89868825+alabak219@users.noreply.github.com>
2021-09-15 15:57:52 -0400

    Demo 2

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def my_function():
+
+    name = input("Tell me your name:")
+
+    print(f'Hello {name}')
 
 
 def print_hi(name):
@@ -11,6 +16,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+   # print_hi('PyCharm')
+    my_function()
+   
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
