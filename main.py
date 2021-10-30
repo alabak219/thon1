4b367e17a8e3993980cd617ab62f49b46a52f44e
alabak219 <89868825+alabak219@users.noreply.github.com>
2021-10-29 20:20:27 -0400

    Adding work to Master Branch

--- main.py
+++ main.py
@@ -44,6 +44,29 @@ for i in range(1, n):
 
 print("\n\nResult:", Sum)
 
+sentence = input("Enter a Sentence: ").lower()
+words = sentence.split()
+
+for i, word in enumerate(words):
+    "if first letter is a vowel"
+    if word[0] in 'aeiou':
+        words[i] = words[i]+ "ay"
+    else:
+        "else get vowel position and postfic all the consonants present before that vowel to the end of work along with ay "
+        has_vowel = False
+
+        for j, letter in enumerate(word):
+            if letter in 'aeiou':
+                words[i] = word[j:] + word[:j] + "ay"
+                has_vowel = True
+                break
+
+        if(has_vowel == False):
+            words[i] = words[i]+ "ay"
+    pig_latin = ' '.join(words)
+    print("Pig Latin: ",pig_latin)
+
+
 
 
 
