f703cc763f4c6d4e844a9ba499b727dc262c16d8
alabak219 <89868825+alabak219@users.noreply.github.com>
2021-11-02 15:20:16 -0400

    work on regex branch

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import re
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+    print("YES! We have a match!")
+else:
+    print("No match")
+
+
+import os
+
+def current_path():
+    print("Current working directory before")
+    print(os.getcwd())
+    print ()
+
+current_path()
+
+os.chdir('../.')
+
+current_path()
+
+
+
+
 
 
 
